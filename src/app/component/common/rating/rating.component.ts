2af0abde7ccb00f3de29a4c015590067ed16026d
Verma <avisverma@deloitte.com>
2019-02-28 10:57:46 +0530

    fixed build issue

--- src/app/component/common/rating/rating.component.ts
+++ src/app/component/common/rating/rating.component.ts
@@ -6,14 +6,34 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent implements OnInit {
-  @Input() rating: string;
+  @Input() rating: number;
   @Input() itemId: string;
+  rating1:boolean;
+  rating2:boolean;
+  rating3:boolean;
+  rating4:boolean;
+  rating5:boolean;
  
 
   inputName: string;
   ngOnInit() {
     this.inputName = this.itemId + '_rating';
     //this.rating = this.rating.
+    if(this.rating == 5){
+      this.rating5 = true;
+    }
+    if(this.rating < 5 || this.rating >=4){
+      this.rating4 = true;
+    }
+    if(this.rating < 4 || this.rating >=3){
+      this.rating3 = true;
+    }
+    if(this.rating < 3 || this.rating >=2){
+      this.rating2 = true;
+    }
+    if(this.rating < 2 || this.rating >=1){
+      this.rating1 = true;
+    }
   }
   
 }
